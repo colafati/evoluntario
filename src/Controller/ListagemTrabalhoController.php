282121656769d67f2bebfe445d9627cb2702ec20
ecolafati <ecolafati@pfbr-ubuntu.pfbr-ubuntu.com>
2018-08-09 15:34:55 -0300

    filto cidade

--- src/Controller/ListagemTrabalhoController.php
+++ src/Controller/ListagemTrabalhoController.php
@@ -3,23 +3,46 @@
 namespace App\Controller;
 
 use App\Entity\Trabalho;
-
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ListagemTrabalhoController extends Controller
 {
     /**
      * @Route("/trabalhos", name="listagem_trabalho")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $repository = $this->getDoctrine()->getRepository(Trabalho::class);
         
-        $em = $this->getDoctrine()->getRepository(Trabalho::class);
-        $trabalhos = $em->findAll();
+        $qb = $repository->createQueryBuilder('t')
+            ->select(array('t.cidade'))
+            ->groupBy('t.cidade')
+            ->orderBy('t.cidade', 'ASC')
+            ->getQuery();
+        $cidades = $qb->execute();
+        
+        $trabalhoEntity = $this->getDoctrine()->getRepository(Trabalho::class);
+        
+        $qb = $repository->createQueryBuilder('t');
+        
+        if(!empty($request->get('cidade'))) {
+            $qb->andWhere('t.cidade = :cidade')
+                ->setParameter('cidade', $request->get('cidade'));
+        }
+        $qb->orderBy('t.id', 'DESC');
+        $trabalhos = $qb->getQuery()->execute();
         
         return $this->render('listagem_trabalho/index.html.twig', [
             'trabalhos' => $trabalhos,
+            'cidades' => $cidades,
+            'request' => array(
+                'cidade' => $request->get('cidade') ?? null,
+                'data' => $request->get('data') ?? null,
+                'texto' => $request->get('texto') ?? null,
+                'categoria' => $request->get('categoria') ?? null,
+            ),
         ]);
     }
 }
