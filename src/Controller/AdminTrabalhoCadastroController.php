ca89312aa714a5fd3a08688c3399aa27ffc85d8f
Caio Parazzi <cparazzi@travelpassgroup.com>
2018-08-09 16:02:40 -0300

    enabling recaptcha

--- src/Controller/AdminTrabalhoCadastroController.php
+++ src/Controller/AdminTrabalhoCadastroController.php
@@ -38,8 +38,9 @@ class AdminTrabalhoCadastroController extends Controller
                 ->getForm();
         
         $form->handleRequest($request);
+        $submit = $form->isSubmitted();
         $confirm = false;
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid() && $this->captchaverify($request->get('g-recaptcha-response'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($trabalho);
             $entityManager->flush();
@@ -47,7 +48,22 @@ class AdminTrabalhoCadastroController extends Controller
         }
 
         return $this->render('admin_trabalho_cadastro/index.html.twig', [
-            'form' => $form->createView(), 'confirm'=>$confirm
+            'form' => $form->createView(), 'confirm'=>$confirm, 'submit'=>$submit
         ]);
-    }    
+    } 
+    
+    function captchaverify($recaptcha){
+            $url = "https://www.google.com/recaptcha/api/siteverify";
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+                "secret"=>"6LcOMmkUAAAAAFKDYuAqwMtToqy_YtZmk-e4fxsK","response"=>$recaptcha));
+            $response = curl_exec($ch);
+            curl_close($ch);
+            $data = json_decode($response);     
+        return $data->success;        
+    }
 }
